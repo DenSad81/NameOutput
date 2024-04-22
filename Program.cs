96177145267d969f2eadc11d609b93621debb404
DenSad81 <tagil81@mail.ru>
2024-04-22 16:45:44 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args) {
+        string character;
+        int quantityAddictionalCharacter = 2;
+        string name;
+        int nameLenght;
+
+        Console.Write("Выберете символ: ");
+        character = Console.ReadLine();
+        Console.Write("Выберете Имя: ");
+        name = Console.ReadLine();
+        nameLenght = name.Length;
+
+        for (int i = 0; i < nameLenght + quantityAddictionalCharacter; i++)
+            Console.Write(character);
+
+        Console.WriteLine();
+        Console.Write(character + name + character);
+        Console.WriteLine();
+
+        for (int i = 0; i < nameLenght + quantityAddictionalCharacter; i++)
+            Console.Write(character);
+    }
+}
